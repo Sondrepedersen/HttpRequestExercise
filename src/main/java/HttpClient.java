071bad99571169702c131df5ac9bb41b44295ff4
Sondrepedersen <Sondreravndal@hotmail.com>
2021-09-14 22:45:57 +0200

    Http 200 ok

---
+++ src/main/java/HttpClient.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class HttpClient {
+
+    private final int statusCode;
+
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host , port);
+        socket.getOutputStream().write(("GET " + requestTarget +
+                " HTTP/1.1\r\nHost: " + host + "\r\n\r\n").getBytes());
+
+        StringBuilder result = new StringBuilder();
+        InputStream in = socket.getInputStream();
+
+        int c;
+        while ((c = in.read()) != -1 && c != '\r') {
+            result.append((char) c);
+        }
+        String myResponseMessage = result.toString();
+        this.statusCode = Integer.parseInt(myResponseMessage.split(" ")[1]);
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpClient client = new HttpClient("httpbin.org" , 80 , "/html");
+        //System.out.println(client.getStatusCode());
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    };
+
+
+}
